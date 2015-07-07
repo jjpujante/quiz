bb1a1ccab49860e05c2e6070a5ab2ba7ae9693ef
jjpujante <jjpujante@gmail.com>
2015-07-07 21:07:20 +0000

    fase 2 tema 6

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
+  res.render('index', { texto: 'El portal donde podrá crear sus propios juegos!' });
 });
 
 module.exports = router;
